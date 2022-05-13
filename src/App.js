b5712436c310e5d561f0027cdf81a91e81894ad9
shahadat2772 <shahadat2772@gmailcom>
2022-05-13 07:49:54 +0600

    project setup complete and navber added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home/Home";
+import Navber from "./pages/Shared/Navber/Navber";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-7xl mx-auto">
+      <Navber></Navber>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/home" element={<Home></Home>}></Route>
+      </Routes>
+      {/* <Routes>
+        <Route path="/home" element={<Home></Home>}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/home" element={<Home></Home>}></Route>
+      </Routes> */}
     </div>
   );
 }
---
+++ src/pages/Shared/Navber/Navber.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Navber = () => {
+  const menuItems = (
+    <>
+      <li>
+        <NavLink
+          className={({ isActive }) => (isActive ? "bg-accent text-white" : "")}
+          to={"/home"}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) => (isActive ? "bg-accent text-white" : "")}
+          to={"/about"}
+        >
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) => (isActive ? "bg-accent text-white" : "")}
+          to={"/appointment"}
+        >
+          Appointment
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) => (isActive ? "bg-accent text-white" : "")}
+          to={"/reviews"}
+        >
+          Reviews
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) => (isActive ? "bg-accent text-white" : "")}
+          to={"/contact"}
+        >
+          Contact
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) => (isActive ? "bg-accent text-white" : "")}
+          to={"/login"}
+        >
+          Login
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menuItems}
+          </ul>
+        </div>
+        <Link to={"/home"} className="btn btn-ghost normal-case text-xl">
+          daisyUI
+        </Link>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navber;
